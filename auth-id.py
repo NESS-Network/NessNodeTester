e1bc6bbc23fe9a8a76ee85e316040c0d9f70d2fc
Aleksej Sokolov <aleksej000@gmail.com>
2022-09-01 03:16:35 +0000

    auth-id.py

---
+++ auth-id.py
@@ -0,0 +1,58 @@
+import json
+import sys
+import urllib.parse
+from lib.NessAuth import NessAuth
+from Crypto.Hash import MD5
+
+
+class Auth:
+
+    data_user = {}
+    data_node = {}
+
+    def __init__(self):
+        return
+
+    def loadUser(self, username: str):
+        filename = username + ".key.json"
+        f = open("out/keys/user/" + filename, "r")
+        return json.loads(f.read())
+
+    def loadNode(self, url: str):
+        filename = urllib.parse.quote_plus(url) + ".key.json"
+        f = open("out/keys/node/" + filename, "r")
+        return json.loads(f.read())
+
+    def run(self, username: str, node_url: str):
+        ness_auth = NessAuth()
+        user = self.loadUser(username)
+        node = self.loadNode(node_url)
+        url = node_url + "/node/test/auth"
+
+        user_private_key = user['keys']["private"][user['keys']['current']]
+
+        h = MD5.new()
+        userhash = username + '-' + node_url + '-' + node["nonce"]
+        h.update(userhash.encode('utf8'))
+        userhash = h.hexdigest()
+
+        print('*** Userhash ***')
+        print(userhash)
+
+        auth_id = ness_auth.auth_id(user_private_key, node_url, node["nonce"], username, user["nonce"])
+
+        print('*** User Auth ID ***')
+        print(auth_id)
+
+        print('*** URL ***')
+        print(url + "/" + userhash + "/" + urllib.parse.quote_plus(auth_id))
+
+        return True
+
+
+if len(sys.argv) == 3:
+    tester = Auth()
+    tester.run(sys.argv[1], sys.argv[2])
+else:
+    print('Show Userhash and AuthID')
+    print('Usage: python auth-id.py <username> <node URL>')
